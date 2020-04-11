166cfaaac0ebeaeb0a9fa563192910f029a740c9
Jon Hill <jon@digitalstate.co.uk>
2020-04-11 19:25:23 +0100

    process xlsx file to java model

--- api/src/main/java/io/github/lbandc/cv19api/XlsDocument.java
+++ api/src/main/java/io/github/lbandc/cv19api/XlsDocument.java
@@ -2,9 +2,16 @@ package io.github.lbandc.cv19api;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.CellAddress;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -18,14 +25,120 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 class XlsDocument {
 
-	void processFile(File file) throws IOException, InvalidFormatException {
-		XSSFWorkbook workbook = new XSSFWorkbook(file);
-		log.debug(String.valueOf(workbook.getActiveSheetIndex()));
-		XSSFSheet sheet = workbook.getSheetAt(0);
-		for (Row row : sheet) {
-			log.info(row.toString());
-		}
-		workbook.close();
+	private final File file;
 
+	static class Model {
+
+		String region;
+		List<Map<String, String>> data = new ArrayList();
+	}
+
+	XlsDocument(File file) {
+		this.file = file;
+	}
+
+	List<Model> processToModel() throws IOException, InvalidFormatException {
+		XSSFWorkbook workbook = new XSSFWorkbook(this.file);
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		CellAddress firstDateCellAddress = this.findFirstDateCellAddress(sheet);
+		CellAddress firstRegionCellAddress = this.findFirstRegionCellAddress(sheet);
+		List<Model> models = new ArrayList<Model>();
+		int startingRow = firstRegionCellAddress.getRow();
+		int lastRow = this.findLastRow(sheet, startingRow);
+
+		for (int i = startingRow; i < lastRow; i++) {
+			var model = this.extractEntryFromStartingRegionCell(sheet, i, firstRegionCellAddress.getColumn(),
+					firstDateCellAddress.getRow());
+			models.add(model);
+		}
+
+		workbook.close();
+		return models;
+
+	}
+
+	private CellAddress findFirstRegionCellAddress(XSSFSheet sheet) throws IOException {
+		for (Row row : sheet) {
+			for (Cell cell : row) {
+				if (this.isRegionCell(cell)) {
+					return cell.getAddress();
+				}
+			}
+		}
+		throw new IOException("Invalid Data Sheet");
+	}
+
+	private int findLastRow(XSSFSheet sheet, int startingRow) throws IOException {
+		int index = startingRow;
+		for (Row row : sheet) {
+			for (Cell cell : row) {
+				if (this.isRegionCell(cell)) {
+					index++;
+
+				}
+			}
+		}
+		return index;
+	}
+
+	private boolean isRegionCell(Cell cell) {
+
+		return cell.getCellType().equals(CellType.STRING) &&
+
+				cell.getStringCellValue().toLowerCase().trim().matches("^(east|london|midlands|north|south).*$");
+
+	}
+
+	private boolean isDateCell(Cell cell) {
+
+		return cell.getCellType().equals(CellType.STRING)
+				&& cell.getStringCellValue().toLowerCase().trim().matches("^([0-9]{2})-.*$");
+
+	}
+
+	private CellAddress findFirstDateCellAddress(XSSFSheet sheet) throws IOException {
+		for (Row row : sheet) {
+			for (Cell cell : row) {
+				if (this.isDateCell(cell)) {
+					return cell.getAddress();
+				}
+			}
+		}
+		throw new IOException("Invalid Data Sheet");
+	}
+
+	private Model extractEntryFromStartingRegionCell(XSSFSheet sheet, int rowIndex, int regionColIndex, int dateRow)
+			throws IOException {
+
+		if (!this.isRegionCell(sheet.getRow(rowIndex).getCell(regionColIndex))) {
+			return null;
+		}
+		Row row = sheet.getRow(rowIndex);
+		Model model = new Model();
+
+		for (Cell cell : row) {
+
+			if (cell.getColumnIndex() < regionColIndex) {
+				continue;
+			}
+
+			if (this.isRegionCell(cell)) {
+				model.region = cell.getStringCellValue();
+
+			} else {
+				Cell dateCell = sheet.getRow(dateRow).getCell(cell.getColumnIndex());
+
+				if (!this.isDateCell(dateCell)) {
+					continue;
+				} else {
+					var map = new HashMap<String, String>();
+					map.put(dateCell.getStringCellValue(), String.valueOf(cell.getNumericCellValue()));
+					model.data.add(map);
+
+				}
+			}
+		}
+
+		return model;
 	}
 }
