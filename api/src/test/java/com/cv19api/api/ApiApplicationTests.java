8db3e08acf1dd05768bde6f7034de30c632a271d
Jon Hill <jon@digitalstate.co.uk>
2020-04-11 14:20:36 +0100

    Initial commit of Spring Boot starter

---
+++ api/src/test/java/com/cv19api/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cv19api.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
