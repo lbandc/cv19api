732359aa812e49c9e8b5199688fa62a0d0992fdb
Jon Hill <jon@digitalstate.co.uk>
2020-04-12 19:44:12 +0100

    Add endpoint (unsecured) to instruct the application to fetch an xlsx source file

--- api/src/main/java/io/github/lbandc/cv19api/ApiV1Controller.java
+++ api/src/main/java/io/github/lbandc/cv19api/ApiV1Controller.java
@@ -1,90 +1,95 @@
 package io.github.lbandc.cv19api;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @RestController
 @RequestMapping(value = "/api/v1", produces = "application/json")
 @AllArgsConstructor
 public class ApiV1Controller {
 
-    private final TrustRepository trustRepository;
+	private final TrustRepository trustRepository;
+	private final FileRetriever fileRetriever;
 
-    @GetMapping(value = {"deaths", "deaths/trusts"})
-    public DeathSummaryResponse<String> deathsByTrust(@RequestParam(value = "date", required = false)
-                                                      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
-        date = todayIfNull(date);
+	@GetMapping(value = { "deaths", "deaths/trusts" })
+	public DeathSummaryResponse<String> deathsByTrust(
+			@RequestParam(value = "date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+		date = todayIfNull(date);
 
-        Collection<TrustRepository.TrustDeaths> deathsByTrust = trustRepository.deathsByTrust(date);
-        Map<String, Integer> deathsByTrustMap = deathsByTrust.stream().collect(Collectors.toMap(
-                TrustRepository.TrustDeaths::getTrust,
-                TrustRepository.TrustDeaths::getDeaths
-        ));
+		Collection<TrustRepository.TrustDeaths> deathsByTrust = trustRepository.deathsByTrust(date);
+		Map<String, Integer> deathsByTrustMap = deathsByTrust.stream().collect(
+				Collectors.toMap(TrustRepository.TrustDeaths::getTrust, TrustRepository.TrustDeaths::getDeaths));
 
-        return new DeathSummaryResponse<>(
-                date,
-                deathsByTrustMap
-        );
-    }
+		return new DeathSummaryResponse<>(date, deathsByTrustMap);
+	}
 
-    @GetMapping("deaths/regions")
-    public DeathSummaryResponse<Region> deathsByRegion(@RequestParam(value = "date", required = false)
-                                                       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
-        date = todayIfNull(date);
+	@GetMapping("deaths/regions")
+	public DeathSummaryResponse<Region> deathsByRegion(
+			@RequestParam(value = "date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+		date = todayIfNull(date);
 
-        Collection<TrustRepository.RegionDeaths> deathsByRegion = trustRepository.deathsByRegion(date);
-        Map<Region, Integer> deathsByRegionMap = deathsByRegion.stream().collect(Collectors.toMap(
-                TrustRepository.RegionDeaths::getRegion,
-                TrustRepository.RegionDeaths::getDeaths
-        ));
+		Collection<TrustRepository.RegionDeaths> deathsByRegion = trustRepository.deathsByRegion(date);
+		Map<Region, Integer> deathsByRegionMap = deathsByRegion.stream().collect(
+				Collectors.toMap(TrustRepository.RegionDeaths::getRegion, TrustRepository.RegionDeaths::getDeaths));
 
-        return new DeathSummaryResponse<>(
-                date,
-                deathsByRegionMap
-        );
-    }
+		return new DeathSummaryResponse<>(date, deathsByRegionMap);
+	}
 
-    @GetMapping("deaths/{from}/{to}")
-    public DeathSummaryResponse<LocalDate> deathsByDay(
-            @PathVariable(value = "from") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
-            @PathVariable(value = "to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to) {
+	@GetMapping("deaths/{from}/{to}")
+	public DeathSummaryResponse<LocalDate> deathsByDay(
+			@PathVariable(value = "from") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+			@PathVariable(value = "to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to) {
 
-        Collection<TrustRepository.DailyDeaths> dailyDeaths = trustRepository.deathsByDay(from, to);
-        Map<LocalDate, Integer> deathsByRegionMap = dailyDeaths.stream().collect(Collectors.toMap(
-                TrustRepository.DailyDeaths::getDate,
-                TrustRepository.DailyDeaths::getDeaths
-        ));
+		Collection<TrustRepository.DailyDeaths> dailyDeaths = trustRepository.deathsByDay(from, to);
+		Map<LocalDate, Integer> deathsByRegionMap = dailyDeaths.stream().collect(
+				Collectors.toMap(TrustRepository.DailyDeaths::getDate, TrustRepository.DailyDeaths::getDeaths));
 
-        return new DeathSummaryResponse<>(
-                to,
-                deathsByRegionMap
-        );
-    }
+		return new DeathSummaryResponse<>(to, deathsByRegionMap);
+	}
 
+	@PostMapping("admin/ingests/{fileDate}")
+	public CommandResponse ingests(
+			@PathVariable(value = "fileDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fileDate) {
+		this.fileRetriever.fetchFile(fileDate);
+		return CommandResponse.OK();
+	}
 
-    private static LocalDate todayIfNull(LocalDate param) {
-        return param == null ? LocalDate.now() : param;
-    }
+	private static LocalDate todayIfNull(LocalDate param) {
+		return param == null ? LocalDate.now() : param;
+	}
 
-    @AllArgsConstructor
-    @Getter
-    private final static class DeathSummaryResponse<T> {
-        private final LocalDate date;
-        private final Map<T, Integer> deaths;
+	@AllArgsConstructor
+	@Getter
+	private final static class DeathSummaryResponse<T> {
+		private final LocalDate date;
+		private final Map<T, Integer> deaths;
 
-        public int getTotalDeaths() {
-            return deaths.values().stream()
-                    .mapToInt(i -> i)
-                    .sum();
-        }
-    }
+		public int getTotalDeaths() {
+			return deaths.values().stream().mapToInt(i -> i).sum();
+		}
+	}
+
+	@AllArgsConstructor
+	@Getter
+	private final static class CommandResponse {
+		private final String status;
+
+		static CommandResponse OK() {
+			return new CommandResponse("OK");
+		}
+	}
 
 }
--- api/src/main/java/io/github/lbandc/cv19api/IngestRepository.java
+++ api/src/main/java/io/github/lbandc/cv19api/IngestRepository.java
@@ -2,20 +2,21 @@ package io.github.lbandc.cv19api;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 public interface IngestRepository extends PagingAndSortingRepository<Ingest, String> {
 
-    @Query("select case when count(i) > 0 then true else false end from Ingest i")
-    boolean existsByUrl(String url);
+	@Query("select case when count(i) > 0 then true else false end from Ingest i where i.url = :url")
+	boolean existsByUrl(@Param("url") String url);
 
-    @Override
-    @RestResource(exported = false)
-    void delete(Ingest ingest);
+	@Override
+	@RestResource(exported = false)
+	void delete(Ingest ingest);
 
-    @Override
-    @RestResource(exported = false)
-    <S extends Ingest> S save(S s);
+	@Override
+	@RestResource(exported = false)
+	<S extends Ingest> S save(S s);
 }
