a5c408ff99b6def90a6262e1dcaefbd44857c064
Jon Hill <jon@digitalstate.co.uk>
2020-04-20 16:35:06 +0100

    add @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST }) to ApiController closes issue #19

--- api/src/main/java/io/github/lbandc/cv19api/ApiV1Controller.java
+++ api/src/main/java/io/github/lbandc/cv19api/ApiV1Controller.java
@@ -6,10 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,7 @@ import lombok.Getter;
 @RestController
 @RequestMapping(value = "/api/v1", produces = "application/json")
 @AllArgsConstructor
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST })
 public class ApiV1Controller {
 
 	private final DeathRecordByTrustRepository deathRepository;
