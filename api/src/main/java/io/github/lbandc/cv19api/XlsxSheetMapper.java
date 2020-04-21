a989b7c6fc4a3dd92559ee4acdd8b640cc5e06c1
Jon Hill <jon@digitalstate.co.uk>
2020-04-21 08:37:46 +0100

    WIP: refactoring

--- api/src/main/java/io/github/lbandc/cv19api/XlsxSheetMapper.java
+++ api/src/main/java/io/github/lbandc/cv19api/XlsxSheetMapper.java
@@ -1,17 +1,43 @@
 package io.github.lbandc.cv19api;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 class XlsxSheetMapper {
 
 	private final XSSFSheet sheet;
 
+	public XlsxSheetMapper(InputStream inputStream, String sheetName) throws IOException {
+		this(new XSSFWorkbook(inputStream), sheetName);
+	}
+
 	public XlsxSheetMapper(final XSSFSheet sheet) {
 		if (null == sheet) {
 			throw new IllegalArgumentException();
+		}
+		this.sheet = sheet;
+	}
+
+	public XlsxSheetMapper(XSSFWorkbook workbook, String sheetName) throws IOException {
+		if (workbook == null) {
+			throw new IOException("Workbook not initialised");
+		}
+		XSSFSheet sheet = null;
+		for (int s = 0; s < workbook.getNumberOfSheets(); s++) {
+			XSSFSheet sht = workbook.getSheetAt(s);
+			if (sht.getSheetName().contains("by trust")) {
+				sheet = sht;
+			}
+
+		}
+		if (sheet == null) {
+			workbook.close();
+			throw new IOException("Not a valid data sheet for " + sheetName);
 		}
 		this.sheet = sheet;
 	}
