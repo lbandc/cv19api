4ae2705690dab2a694647b6d1a5beafc055084db
Will Faithfull <will@sourcepulp.com>
2020-04-21 08:32:57 +0100

    Point swagger requests to cv19api.com

--- api/src/main/java/io/github/lbandc/cv19api/ApiApplication.java
+++ api/src/main/java/io/github/lbandc/cv19api/ApiApplication.java
@@ -1,5 +1,7 @@
 package io.github.lbandc.cv19api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +12,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableJpaAuditing
 @EnableAsync
+@OpenAPIDefinition(servers = {
+		@Server(url = "https://api.cv19api.com", description = "cv19api")
+})
 public class ApiApplication {
 
 	public static void main(String[] args) {
